a038a5df383e2c62ec8d7c4d6d76e15b0c3e8653
Backend Learning <shivasainath27122002@gmail.com>
2025-11-17 21:55:26 +0530

    added validation

--- src/LoopDemo.java
+++ src/LoopDemo.java
@@ -7,8 +7,13 @@ public class LoopDemo {
         long vstart = sc.nextLong();
         System.out.println("Enter Loop Ending point :");
         long vend = sc.nextLong();
-        for(long l=vstart; l<=vend ; l++){
-            System.out.println(l);
+        if(vend < vstart){
+            System.out.println("Starting number cannot be greater than the Ending number.");
+        }
+        else {
+            for (long l = vstart; l <= vend; l++) {
+                System.out.println(l);
+            }
         }
     }
 }
